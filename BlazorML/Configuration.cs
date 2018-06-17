8d8c3f29f490d0a9238b8980c6802ee257a0b0d1
Daniel Sandberg <bjorndaniel@gmail.com>
2018-06-17 21:29:35 +0200

    Delete Configuration.cs

--- BlazorML/Configuration.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BlazorML
-{
-    public static class Configuration
-    {
-        public static string CustomVisionUrl => "YOUR-CUSTOM-VISION-URL";
-        public static string PredictionKey => "YOUR-CUSTOM-VISION-PREDICTION-KEY";
-        public static string ContentType => "application/octet-stream";
-    }
-}
